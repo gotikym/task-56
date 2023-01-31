7cf102684c719a67b41d3e2fac12e404b0c850df
gotikym <gotikym9@mail.ru>
2023-01-31 22:11:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        StewSet stewSet = new StewSet();
+        stewSet.ShowOverdue();
+    }
+}
+
+class StewSet
+{
+    private DateTime _today = DateTime.Today;
+    private List<Stew> _stew = new List<Stew>();
+
+    public StewSet()
+    {
+        Add();
+    }
+
+    public void ShowOverdue()
+    {
+        var overdueStew = _stew.Where(stew => stew.ProductionYear + stew.ShelfLife < _today.Year);
+
+        foreach(Stew stew in overdueStew)
+        {
+            Console.WriteLine(stew.Name);
+        }
+    }
+
+    private void Add()
+    {
+        _stew.Add(new Stew("Гродфуд", 2019 ));
+        _stew.Add(new Stew("Барс", 2021 ));
+        _stew.Add(new Stew("Совок", 2023 ));
+        _stew.Add(new Stew("Йошкар-Олинский мясокомбинат", 2020 ));
+        _stew.Add(new Stew("Вкусвилл", 2016 ));
+        _stew.Add(new Stew("Микоян", 2020 ));
+        _stew.Add(new Stew("Гродфуд", 2021 ));
+        _stew.Add(new Stew("Барс", 2022 ));
+        _stew.Add(new Stew("Микоян", 2017 ));
+        _stew.Add(new Stew("Совок", 2021 ));
+        _stew.Add(new Stew("Вкусвилл", 2022 ));
+        _stew.Add(new Stew("Йошкар-Олинский мясокомбинат", 2018 ));
+    }
+}
+
+class Stew
+{
+    public Stew(string name, int productionYear)
+    {
+        Name = name;
+        ProductionYear = productionYear;
+        ShelfLife = 5;
+    }
+
+    public string Name { get; private set; }
+    public int ProductionYear { get; private set; }
+    public int ShelfLife { get; private set; }
+}
