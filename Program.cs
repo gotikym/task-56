afcc69417dcb10d122ea9aee6069723d3f227fcf
Ivan Malkovskii <103892616+gotikym@users.noreply.github.com>
2023-01-31 22:19:03 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -7,8 +7,24 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        const string CommandLookOverdue = "1";
+        const string CommandGoHome = "2";
+        bool isExit = false;
+
         StewSet stewSet = new StewSet();
-        stewSet.ShowOverdue();
+
+        Console.WriteLine("Доброго дня, сотрудник");
+
+        while (isExit == false)
+        {
+            Console.WriteLine("Проверить просрочку по тушенке - " + CommandLookOverdue + ", пойти домой - " + CommandGoHome);
+            string userChoice = Console.ReadLine();
+
+            if (userChoice == CommandLookOverdue)
+                stewSet.ShowOverdue();
+            else if (userChoice == CommandGoHome)
+                isExit = true;
+        }
     }
 }
 
@@ -26,7 +42,7 @@ class StewSet
     {
         var overdueStew = _stew.Where(stew => stew.ProductionYear + stew.ShelfLife < _today.Year);
 
-        foreach(Stew stew in overdueStew)
+        foreach (Stew stew in overdueStew)
         {
             Console.WriteLine(stew.Name);
         }
@@ -34,18 +50,18 @@ class StewSet
 
     private void Add()
     {
-        _stew.Add(new Stew("Гродфуд", 2019 ));
-        _stew.Add(new Stew("Барс", 2021 ));
-        _stew.Add(new Stew("Совок", 2023 ));
-        _stew.Add(new Stew("Йошкар-Олинский мясокомбинат", 2020 ));
-        _stew.Add(new Stew("Вкусвилл", 2016 ));
-        _stew.Add(new Stew("Микоян", 2020 ));
-        _stew.Add(new Stew("Гродфуд", 2021 ));
-        _stew.Add(new Stew("Барс", 2022 ));
-        _stew.Add(new Stew("Микоян", 2017 ));
-        _stew.Add(new Stew("Совок", 2021 ));
-        _stew.Add(new Stew("Вкусвилл", 2022 ));
-        _stew.Add(new Stew("Йошкар-Олинский мясокомбинат", 2018 ));
+        _stew.Add(new Stew("Гродфуд", 2019));
+        _stew.Add(new Stew("Барс", 2021));
+        _stew.Add(new Stew("Совок", 2023));
+        _stew.Add(new Stew("Йошкар-Олинский мясокомбинат", 2020));
+        _stew.Add(new Stew("Вкусвилл", 2016));
+        _stew.Add(new Stew("Микоян", 2020));
+        _stew.Add(new Stew("Гродфуд", 2021));
+        _stew.Add(new Stew("Барс", 2022));
+        _stew.Add(new Stew("Микоян", 2017));
+        _stew.Add(new Stew("Совок", 2021));
+        _stew.Add(new Stew("Вкусвилл", 2022));
+        _stew.Add(new Stew("Йошкар-Олинский мясокомбинат", 2018));
     }
 }
 
